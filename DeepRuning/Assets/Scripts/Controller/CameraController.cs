0318005bf19cfefa173eb83d302d4630124cf809
lovelyapple <lovelyappleoffice@gmail.com>
2018-10-08 23:28:06 +0900

    VRvalid

--- DeepRuning/Assets/Scripts/Controller/CameraController.cs
+++ DeepRuning/Assets/Scripts/Controller/CameraController.cs
@@ -12,6 +12,10 @@ public class CameraController : MonoBehaviour
         VRModePC,
         VRModeDev,
     };
+    [SerializeField] Transform eyeLeft;
+    [SerializeField] Transform eyeRight;
+    [Range(0.1f, 4f)]
+    public float eyeDistance = 0.5f;
     public CameraMode cameraMode;
     [SerializeField] Vector3 followDiff;
     [SerializeField] Vector3 simpleFollowLookTarget;
@@ -20,18 +24,32 @@ public class CameraController : MonoBehaviour
     Quaternion startRotation;
     Quaternion currentRotation;
     [SerializeField] Text uiLabel;
+    [SerializeField] Gyroscope gyroscope;
     void Start()
     {
         UpdateCameraSettings();
+
+
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("gyroscope no sup");
+        }
     }
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
-    void Update()
-    {
-        var a = Input.acceleration;
-        this.transform.rotation = new Quaternion(a.x, a.y, a.z, 0);
-    }
+    // void Update()
+    // {
+    //     var a = Input.acceleration;
+    //     var text = string.Format("ACC X : {0:.00}, Y : {1:.00} X :{2:.00} ¥n", a.x, a.y, a.z);
+    //     //this.transform.rotation = new Quaternion(a.x, a.y, a.z, 0);
+    //     uiLabel.text = text;
+    // }
     void UpdateCameraSettings()
     {
     }
@@ -39,24 +57,53 @@ public class CameraController : MonoBehaviour
     /// <summary>
     /// Update is called every frame, if the MonoBehaviour is enabled.
     /// </summary>
-    // void Update()
-    // {
-    //     currentRotation = Input.gyro.attitude;
-    //     uiLabel.text = string.Format("Gyro x{0:0.000},y{1:0.000},z{2:0.000}", currentRotation.x, currentRotation.y, currentRotation.z);
+    void Update()
+    {
+        currentRotation = Input.gyro.attitude;
+        uiLabel.text = string.Format("Gyro x{0:0.000},y{1:0.000},z{2:0.000}", -Input.gyro.rotationRate.x, -Input.gyro.rotationRate.y, -Input.gyro.rotationRate.z);
 
-    //     switch (cameraMode)
-    //     {
-    //         case CameraMode.SimpleFollow:
-    //             transform.position = player.transform.position + followDiff;
-    //             var lookAtTarget = player.transform.position + simpleFollowLookTarget;
-    //             transform.LookAt(lookAtTarget);
-    //             break;
-    //         case CameraMode.LerpFollow:
-    //             break;
-    //         case CameraMode.VRModePC:
-    //             break;
-    //         case CameraMode.VRModeDev:
-    //             break;
-    //     }
-    // }
+        if (eyeLeft != null)
+        {
+            if (Input.GetKey(KeyCode.O))
+            {
+                eyeDistance += 0.01f;
+            }
+            else if (Input.GetKey(KeyCode.P))
+            {
+                eyeDistance -= 0.01f;
+            }
+
+            eyeDistance = Mathf.Min(0, eyeDistance);
+            if ((-eyeLeft.localPosition.x != eyeDistance / 2f) || (eyeRight.localPosition.x != eyeDistance / 2f))
+            {
+                var localLF = eyeLeft.localPosition;
+                localLF.x = -eyeDistance / 2f;
+                eyeLeft.localPosition = localLF;
+
+                var localRI = eyeRight.localPosition;
+                localRI.x = eyeDistance / 2f;
+                eyeRight.localPosition = localRI;
+            }
+        }
+
+        switch (cameraMode)
+        {
+            case CameraMode.SimpleFollow:
+                transform.position = player.transform.position + followDiff;
+                //transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, -Input.gyro.rotationRateUnbiased.z);
+                //transform.Rotate(-Input.gyro.rotationRate.x, -Input.gyro.rotationRate.y, -Input.gyro.rotationRate.z);
+                //transform.rotation = new Quaternion(-Input.gyro.userAcceleration.x, -Input.gyro.userAcceleration.y, -Input.gyro.userAcceleration.z, 1);
+                // var lookAtTarget = player.transform.position + simpleFollowLookTarget;
+                // transform.LookAt(lookAtTarget);
+                Quaternion direction = Input.gyro.attitude;
+                transform.rotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(-direction.x, -direction.y, direction.z, direction.w));
+                break;
+            case CameraMode.LerpFollow:
+                break;
+            case CameraMode.VRModePC:
+                break;
+            case CameraMode.VRModeDev:
+                break;
+        }
+    }
 }
